d31f3681d0257f00ee40c68fb4469501eda1cfaf
Doyin Olarewaju <youngenterpreneur11@gmail.com>
2023-05-23 02:30:32 -0600

    chore(dependencies): Installed and setup for swr, trpc, airtable, axios, zod

---
+++ config/envs.ts
@@ -0,0 +1,8 @@
+const envs = {
+  webflow: {
+    webflowClientId: process.env.WEBFLOW_CLIENT_ID,
+    webflowAuthUrl: process.env.WEBFLOW_AUTH_URL,
+  },
+};
+
+export default envs;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps, AppType } from 'next/app';
+import { trpc } from '../types/trpc';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: AppType = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default trpc.withTRPC(App);
